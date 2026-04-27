fba7f8542a2ba5124ab69637a82b076697c66b93
safatalnur <safatalnur@gmail.com>
2026-04-27 00:32:34 -0400

    refractor: redirect view from Controller instead of directly from route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index']);
