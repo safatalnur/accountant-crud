d103423662f07d5686dbdcae4be78a83f69ebd24
safatalnur <safatalnur@gmail.com>
2026-04-27 00:17:28 -0400

    FEATURE: add font-awesome cdn

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -22,6 +22,8 @@
         @yield('content')
     </main>
 
+    <!-- Font-Awesome -->
+    <script src="https://kit.fontawesome.com/11d88d03e3.js" crossorigin="anonymous"></script>
     <!-- Bootstrap JavaScript -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
