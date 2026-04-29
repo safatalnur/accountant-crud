3a80bff56008ee292d3550a28d48fd41d21fff88
safatalnur <safatalnur@gmail.com>
2026-04-29 00:25:41 -0400

    FEATURE: add footer and styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,45 @@
-<footer>
-    
+<footer class="bg-dark">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-sm d-flex flex-column">
+                <h2 class="text-white text-uppercase font-small">Why SHProfessional</h2>
+                <hr class="teal w-25">
+                <p class="text-white font-small">Have 30 years of combined experience helping our clients</p>
+                <button type="button" class="btn btn-outline-light btn-sm">Get Started</button>
+            </div>
+            <div class="col-sm d-flex flex-column">
+                <h2 class="text-white text-uppercase font-small">Services</h2>
+                <hr class="teal w-25">
+                <a class="text-white font-small" href="">Accounting</a>
+                <a class="text-white font-small" href="">Taxation</a>
+            </div>
+            <div class="col-sm d-flex flex-column">
+                <h2 class="text-white text-uppercase font-small">Links</h2>
+                <hr class="teal w-25">
+                <a class="text-white font-small" href="">Why SHProfessional</a>
+                <a class="text-white font-small" href="">Partners</a>
+                <a class="text-white font-small" href="">Contact Us</a>
+            </div>
+            <div class="col-sm d-flex flex-column">
+                <h2 class="text-white text-uppercase font-small">Contact</h2>
+                <hr class="teal w-25">
+                <div class="d-flex align-items-start mb-2">
+                    <i class="fa-solid fa-house text-white mt-1"></i>
+                    <span class="text-white pl-4 font-small">
+                        2972 Danforth Ave, East York<br>Ontario M4C 1M6
+                    </span>
+                </div>
+
+                <div class="d-flex align-items-center mb-2">
+                    <i class="fa-solid fa-envelope text-white"></i>
+                    <a class="text-white pl-4 font-small" href="mailto:info@shprofessional.ca">info@shprofessional.ca</a>
+                </div>
+
+                <div class="d-flex align-items-center">
+                    <i class="fa-solid fa-phone text-white"></i>
+                    <span class="text-white pl-4 font-small">647-351-0622 | 647-348-9022</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
