6d7f0de3ae1cf4a0a705b2219b836137d212efb0
safatalnur <safatalnur@gmail.com>
2026-04-29 01:23:36 -0400

    CHORE: populate homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,20 @@
 @extends('layout.layout')
 
 @section('content')
-<section>
-    <p>This is the homepage</p>
+<section id="home-hero" class="container-fluid d-flex justify-content-center">
+    <div class="container-md d-flex justify-content-center">
+        <div class="row">
+            <div class="col-sm p-5 text-white text-center d-flex flex-column justify-content-center">
+                <h1 class="display-5">30 Years of Experience</h1>
+                <p>
+                    Our tax professionals help clients meet regulatory obligations, reduce tax burdens, and navigate complex regulations around the world.
+                </p>
+                <button type="button" class="btn btn-success">Register Now</button>
+            </div>
+            <div class="col-sm p-5 text-white d-flex flex-column justify-content-center">
+                One of three columns
+            </div>
+        </div>
+    </div>
 </section>
 @endsection
